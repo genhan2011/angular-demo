f2300e3eef443042b20fb49c9d485d5219f0fc35
Genhan Chen <genhan.chen@gmail.com>
2014-10-20 13:16:25 -0700

    Binde data to view

--- controller/controller.js
+++ controller/controller.js
@@ -1,7 +1,14 @@
 (function(){
     var app = angular.module('controller', []);
 
-    app.controller('PresenterController', ['$scope', function($scope){
+    app.controller('PresenterController', ['$scope', '$http', function($scope, $http){
         $scope.presenter = 'Genhan Chen';
+
+        $scope.people = [];
+        $http.get('data/people.json').success(function(resp, status){
+            $scope.people = resp.data;
+        }).error(function(err, status){
+            console.log(err);
+        });
     }])
 })();
