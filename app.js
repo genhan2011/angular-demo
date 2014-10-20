2f76ea3e8332d5680ea321852e2025dc9243d832
Genhan Chen <genhan.chen@gmail.com>
2014-10-20 12:08:55 -0700

    Separate view from index.html

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 (function(){
     var app = angular.module('cpmsTeam', [
+        'ngRoute',
         'controller'
     ]);
+
+    app.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'view/view.html'
+        }).otherwise({
+            redirectTo: '/'
+        });
+    }]);
 })();
